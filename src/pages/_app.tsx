45065acc6eac39e7c91e933c7675342458bd8fce
Cody De Arkland <codydearkland@gmail.com>
2023-04-11 15:21:15 -0700

    adjusting configs for toplevel await

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import "@/styles/globals.css";
 import React from "react";
 import type { AppProps } from "next/app";
@@ -5,17 +7,43 @@ import { asyncWithLDProvider } from "launchdarkly-react-client-sdk";
 import dynamic from "next/dynamic";
 import { globalStyles } from "@/stitches.config";
 import Context from "@/context/state";
+import {v4 as uuid} from 'uuid'
+import {osName, isMobile} from 'react-device-detect'
 
 const CartWithoutSSR = dynamic(() => import("../components/cart"), {
   ssr: false,
 });
 
 let c;
-
 if (typeof window !== "undefined") {
-  const LDProvider = await asyncWithLDProvider({
+    const LDProvider = await asyncWithLDProvider({
     clientSideID: process.env.NEXT_PUBLIC_LD_CLIENT_KEY,
+    context: {
+      kind: "multi",
+      "user": 
+      {
+        key: uuid(),
+        user: "anonymous",
+      },
+      "session":
+      {
+        key: uuid(),
+        name: "new session",
+        description: "session data that we use for experimentation"
+      },
+      "device":
+      {
+        key: uuid(),
+        operating_system: osName,
+        mobile_device: isMobile
+      }, 
+      "location": {
+        key: uuid(),
+        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone
+      }
+    },
   });
+
   c = ({ Component, pageProps }: AppProps) => {
     globalStyles();
     return (
@@ -28,8 +56,6 @@ if (typeof window !== "undefined") {
       </Context>
     );
   };
-} else {
-  c = () => "window is undefined for some reason";
-}
+} 
 
 export default c;
