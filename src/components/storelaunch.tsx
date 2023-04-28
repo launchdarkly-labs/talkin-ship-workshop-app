eef8e26c75db1ff90c5b37929bad2ff04b0bf593
Cody De Arkland <codydearkland@gmail.com>
2023-04-28 14:37:11 -0700

    Shipping new change

--- src/components/storelaunch.tsx
+++ src/components/storelaunch.tsx
@@ -9,7 +9,7 @@ const StoreContent = () => (
     <div className="flex text-4xl border-b-4 border-orange-600 w-full md:text-6xl lg:text-8xl text-yellow-500 mx-auto place-items-center place-content-center">
       <div className="">
           <Image
-            src="/high-five.png"
+            src="/Toggle-5-Clipped.png"
             alt="Toggle"
             width={325}
             height={325}
@@ -18,7 +18,8 @@ const StoreContent = () => (
         </div>
         <div>
         <p className="font-bold text-xl md:text-3xl lg:text-5xl text-black">The New </p>
-        <p className="font-bold text-transparent bg-clip-text bg-gradient-to-br from-orange-300 to-orange-600 pb-4">Toggle Outfitters! </p>
+        <p className="font-bold text-transparent bg-clip-text bg-gradient-to-b from-orange-300 to-orange-600 pb-4">Toggle Outfitters! </p>
+        {/* Future adspace area for experiment */}
         </div>
     </div>
 
