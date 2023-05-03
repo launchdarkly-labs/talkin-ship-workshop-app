52c605c0bbca819ac972255f60e7ea2b82ca4a0c
Cody De Arkland <codydearkland@gmail.com>
2023-05-03 13:46:03 -0700

    clearing out unused things

--- src/components/ui/ReserveButton.tsx
+++ src/components/ui/ReserveButton.tsx
@@ -1,16 +1,4 @@
-import { useState } from "react";
-import { EmailField, NameField } from "./FormFields";
-import {
-  FormRoot,
-  AlertDialogOverlay,
-  AlertDialogContent,
-  AlertDialogTitle,
-  AlertDialogDescription,
-  Flex,
-} from "../component-library";
 import { Button } from "@/components/ui/button";
-import * as AlertDialog from "@radix-ui/react-alert-dialog";
-import * as Form from "@radix-ui/react-form";
 import {
   Dialog,
   DialogContent,
@@ -80,57 +68,6 @@ const ReserveButton = ({
       </DialogContent>
       )}
     </Dialog>
-    // <AlertDialog.Root>
-    //   <AlertDialog.Trigger>
-    //     <button className='grid my-4 bottom-0 bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 border rounded-xl'
-    //       onClick={() => {
-    //         setHandleModal(true);
-
-    //       }}
-    //     >
-    //       Reserve Yours!
-    //     </button>
-    //   </AlertDialog.Trigger>
-    //   {handleModal && (
-    //     <AlertDialog.Portal>
-    //       <AlertDialogOverlay />
-    //       <AlertDialogContent>
-    //         <FormRoot onSubmit={handleClickTest}>
-    //           <AlertDialogTitle>
-    //             Thanks for your interest in our Toggles!
-    //           </AlertDialogTitle>
-    //           <AlertDialogDescription>
-    //             Please provide your contact info and our Toggle
-    //             Specialists will contact you in 3-5 business days
-    //           </AlertDialogDescription>
-    //           <NameField
-    //           value={name}
-    //           onChange={(event) => updateField("name", event)}
-    //         />
-    //         <EmailField
-    //           value={email}
-    //           onChange={(event) => updateField("email", event)}
-    //         />
-    //           <Flex css={{ justifyContent: "flex-end" }}>
-    //             <AlertDialog.Cancel asChild>
-    //               <Button css={{ marginRight: 25 }}>Cancel</Button>
-    //             </AlertDialog.Cancel>
-    //             <Form.Submit asChild onSubmit={(e) => handleClickTest(e)}>
-    //               <Button
-    //                 variant="green"
-    //                 onClick={() => {
-    //                   onButtonClick();
-    //                 }}
-    //               >
-    //                 Submit
-    //               </Button>
-    //             </Form.Submit>
-    //           </Flex>
-    //         </FormRoot>
-    //       </AlertDialogContent>
-    //     </AlertDialog.Portal>
-    //   )}
-    // </AlertDialog.Root>
   );
 };
 
